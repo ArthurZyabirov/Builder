4ac4dffefd019fa83c0d0aa11c0583e098ddbe09
Arthur Zyabirov <zyabirow@gmail.com>
2021-11-14 01:29:21 +0300

    work done

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,44 +1,16 @@
+import java.util.Objects;
 import java.util.OptionalInt;
 
 public class Person {
     protected final String name;
     protected final String surname;
-    protected int age;
-    protected String city;
+    private OptionalInt age = OptionalInt.of(-1);
+    private String address;
 
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
-
-    public Person(String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
-    public Person(String name, String surname, int age, String city) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.city = city;
-    }
-
-    public Person(String name, String surname, String city) {
-        this.name = name;
-        this.surname = surname;
-        this.city = city;
-    }
-
-    //    public Person(String name, String surname) {
-//        //...
-//    }
-//
-//    public Person(String name, String surname, int age) {
-//        //...
-//    }
-    public boolean hasAge() { if (age > 0) return true; }
-    public boolean hasAddress() { /*...*/ }
 
     public String getName() {
         return name;
@@ -48,19 +20,64 @@ public class Person {
         return surname;
     }
 
-    public OptionalInt getAge() {
-        return OptionalInt.of(age);
+    public int getAge() {
+        return age.getAsInt();
     }
 
-    public String getAddress() { /*...*/ }
 
-    public String setAddress(String address) { /*...*/ }
-    public void happyBirthday() { /*...*/ }
+    public void setAge(int age) {
+        if (this.age.getAsInt() == -1)
+            this.age = OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if (age.getAsInt() != -1)
+            age = OptionalInt.of(age.getAsInt() + 1);
+    }
+
+    public boolean hasAge() {
+        return age.isPresent();
+    }
+
+    public boolean hasAddress() {
+        return !address.isEmpty();
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setAge(0)
+                .setSurname(surname)
+                .setAddress(address);
+    }
 
     @Override
-    public String toString() { /*...*/ }
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
 
     @Override
-    public int hashCode() { /*...*/ }
-    public PersonBuilder newChildBuilder() { /*...*/ }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return name.equals(person.name) && surname.equals(person.surname) && age.equals(person.age) && Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, address);
+    }
 }
--- src/main/java/PersonBuilder.java
+++ src/main/java/PersonBuilder.java
@@ -1,11 +1,44 @@
+import java.util.OptionalInt;
+
 public class PersonBuilder {
-    //...
+    private String name;
+    private String surname;
+    private OptionalInt age;
+    private String address;
 
-    public PersonBuilder setName(String name) { /*...*/ }
-    public PersonBuilder setSurname(String surname) { /*...*/ }
-    public PersonBuilder setAge(int age) { /*...*/ }
-    public PersonBuilder setAddress(String address) { /*...*/ }
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
 
-    public Person build() { /*...*/ }
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0)
+            throw new IllegalArgumentException();
+
+        this.age = OptionalInt.of(age);
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null)
+            throw new IllegalStateException();
+
+        Person p = new Person(name, surname);
+        if (age.getAsInt() != -1)
+            p.setAge(age.getAsInt());
+        if (!address.isEmpty())
+            p.setAddress(address);
+
+        return p;
+    }
 }
-
